a36cc79efa006ee45b23d29abe7f95811d48640e
Seb35 <seb35wikipedia@gmail.com>
2014-12-25 12:33:36 +0100

    Suite renommage répertoires

--- scripts/liste-paquets.sh
+++ scripts/liste-paquets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname $(pwd)/$0)/.."
-grep -h import archéo-lex legifrance/*.py | grep -v '#import' | grep -v legifrance | sort | uniq | sed 's/^import //' | sed 's/from \([^ ]*\) import .*/\1/' | sed 's/\..*//' | sort | uniq
+grep -h import archeo-lex marcheolex/*.py | grep -v '#import' | grep -v marcheolex | sort | uniq | sed 's/^import //' | sed 's/from \([^ ]*\) import .*/\1/' | sed 's/\..*//' | sort | uniq
