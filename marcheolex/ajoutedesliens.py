1962ff8eecb4dd428a0ddf1b80348e0b73425f2c
Seb35 <seb35@seb35.fr>
2018-03-10 17:01:11 +0100

    Embrayon de reconnaissance de liens

---
+++ marcheolex/ajoutedesliens.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# 
+# Archéo Lex – Pure Histoire de la Loi française
+# 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# the LICENSE file for more details.
+
+# Imports
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+import subprocess
+import datetime
+import time
+import re
+from pytz import timezone
+from string import strip, join
+from path import Path
+from bs4 import BeautifulSoup
+import legi
+import legi.utils
+
+
+def ajoute_liens( texte ):
+
+        """
+        Rechercher des liens dans un texte.
+
+        :param texte:
+            (string) Texte où seront ajouté les liens.
+        :returns:
+            (liste de tuples) Chaque tuple comprend : le texte de la capture, puis les parties d’identification.
+        """
+
+        liens = []
+
+        # Constitution (en vigueur à la date du texte)
+        results = re.findall( r'(articles? ?([0-9.\-a-z]+)) (de la )(constitution)', texte, re.IGNORECASE )
+        for result in results:
+            liens.append( ( result[0], 'constitution', result[1] ) )
+
+        return liens
+
+# vim: set ts=4 sw=4 sts=4 et:
